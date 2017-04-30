83aa4c1a2b127bd93fde76b5473683b802c0cdf6
Tom Holland <codesman@hackattic.net>
2017-04-29 21:48:21 -0700

    Detail View

--- StormViewer/StormViewer/SourceViewController.swift
+++ StormViewer/StormViewer/SourceViewController.swift
@@ -39,4 +39,13 @@ class SourceViewController: NSViewController, NSTableViewDataSource, NSTableView
         
         return vw
     }
+    
+    func tableViewSelectionDidChange(_ notification: Notification) {
+        guard tableView.selectedRow != -1 else { return }
+        guard let splitVC = parent as? NSSplitViewController else { return }
+        
+        if let detail = splitVC.childViewControllers[1] as? DetailViewController {
+            detail.imageSelected(name: pictures[tableView.selectedRow])
+        }
+    }
 }
