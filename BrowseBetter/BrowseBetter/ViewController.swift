00ac1b45d8d236e15f7530966e4c4902e8ade0a3
Tom Holland <codesman@hackattic.net>
2017-04-30 15:29:33 -0700

    BrowseBetter - Initial Commit

---
+++ BrowseBetter/BrowseBetter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BrowseBetter
+//
+//  Created by Tom Holland on 4/30/17.
+//  Copyright © 2017 Tom Holland. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
