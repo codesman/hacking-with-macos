79fd6711195afb9ca8b0e711c4b261da8b96b6ac
Tom Holland <codesman@hackattic.net>
2017-04-30 12:19:28 -0700

    Moved sharedClick()

--- StormViewer/StormViewer/ViewController.swift
+++ StormViewer/StormViewer/ViewController.swift
@@ -16,21 +16,10 @@ class ViewController: NSSplitViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func sharedClick(_ sender: NSView) {
-        guard let detail = childViewControllers[1] as? DetailViewController else { return }
-        guard let image = detail.imageView.image else { return }
-        
-        let picker = NSSharingServicePicker(items: [image])
-        picker.show(relativeTo: .zero, of: sender, preferredEdge: .minY)
-        
-    }
-    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
-
-
 }
 
--- StormViewer/StormViewer/WindowController.swift
+++ StormViewer/StormViewer/WindowController.swift
@@ -18,4 +18,12 @@ class WindowController: NSWindowController {
        shareButton.sendAction(on: .leftMouseDown)
     }
 
+    @IBAction func sharedClick(_ sender: NSView) {
+        guard let split = contentViewController as? NSSplitViewController else { return }
+        guard let detail = split.childViewControllers[1] as? DetailViewController else { return }
+        guard let image = detail.imageView.image else { return }
+        
+        let picker = NSSharingServicePicker(items: [image])
+        picker.show(relativeTo: .zero, of: sender, preferredEdge: .minY)
+    }
 }
