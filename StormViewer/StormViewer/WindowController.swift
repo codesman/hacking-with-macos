74a1b699c08a859ce8c4ffdf8f6cd4fec7c85413
Tom Holland <codesman@hackattic.net>
2017-04-30 12:13:22 -0700

    Add WindowController

---
+++ StormViewer/StormViewer/WindowController.swift
@@ -0,0 +1,19 @@
+//
+//  WindowController.swift
+//  StormViewer
+//
+//  Created by Tom Holland on 4/30/17.
+//  Copyright © 2017 Tom Holland. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
