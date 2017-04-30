3b937b50c3b043edefc55bac9a8d758293392b73
Tom Holland <codesman@hackattic.net>
2017-04-29 19:31:41 -0700

    Initial Commit

---
+++ StormViewer/StormViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StormViewer
+//
+//  Created by Tom Holland on 4/29/17.
+//  Copyright © 2017 Tom Holland. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
