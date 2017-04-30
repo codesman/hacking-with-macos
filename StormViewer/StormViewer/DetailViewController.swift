47264320fbc2491a4e7b28b06d2e89036cde2f82
Tom Holland <codesman@hackattic.net>
2017-04-29 21:17:18 -0700

    Added Outlets

---
+++ StormViewer/StormViewer/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  StormViewer
+//
+//  Created by Tom Holland on 4/29/17.
+//  Copyright © 2017 Tom Holland. All rights reserved.
+//
+
+import Cocoa
+
+class DetailViewController: NSViewController {
+
+    @IBOutlet var imageView: NSImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ StormViewer/StormViewer/SourceViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SourceViewController.swift
+//  StormViewer
+//
+//  Created by Tom Holland on 4/29/17.
+//  Copyright © 2017 Tom Holland. All rights reserved.
+//
+
+import Cocoa
+
+class SourceViewController: NSViewController {
+
+    @IBOutlet var tableView: NSTableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
