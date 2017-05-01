2009f3fb11a4825a52797c995e748889435186ae
Tom Holland <codesman@hackattic.net>
2017-05-01 07:28:06 -0700

    Add WindowController

---
+++ BrowseBetter/BrowseBetter/WindowController.swift
@@ -0,0 +1,19 @@
+//
+//  WindowController.swift
+//  BrowseBetter
+//
+//  Created by Tom Holland on 5/1/17.
+//  Copyright © 2017 Tom Holland. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
